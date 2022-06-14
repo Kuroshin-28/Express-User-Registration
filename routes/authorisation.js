44c8ef326ef6136a598aa20af13e9c5c69c4dad7
Kuroshin28 <baptiste.bertinWOT@gmail.com>
2022-06-14 12:21:51 +0200

    Add a correction on the authentification that couldn't handle worng passwords.

--- routes/authorisation.js
+++ routes/authorisation.js
@@ -4,24 +4,27 @@ const Auth = require('../models/user');
 authRouter.post('/checkCredentials', (req, res) => {
     const { email, password } = req.body;
     Auth.findByEmail(email).then((user) => {
-        if (!Auth.verifyPassword(password, user.hashedPassword)) {
-            return Promise.reject('INVALID_PASSWORD');
-        }
-        res.status(200).json({
-            "firstname" : user.firstname,
-            "lastname" : user.lastname,
-            "city" : user.city,
-            "language" : user.language,
-            "email" : user.email
-        });
-    }).catch((err) => {
-        console.error(err);
-        if (err === 'INVALID_PASSWORD') {
-            res.status(401).json({ message: 'Wrong password or email' });
+        if (!user) {
+            console.log("User Authentification : Failure");
+            res.status(401).send('Wrong email or password');
         } else {
-            res.status(500).send('Cant reach the server for authentification');
+            Auth.verifyPassword(password, user.hashedPassword).then((accessGranted) => {
+                if (!accessGranted) {
+                    console.log("User Authentification : Failure");
+                    res.status(401).send('Wrong email or password');
+                } else {
+                    console.log("User Authentification : Success");
+                    res.status(200).json({
+                        "firstname" : user.firstname,
+                        "lastname" : user.lastname,
+                        "city" : user.city,
+                        "language" : user.language,
+                        "email" : user.email
+                    });
+                }
+            })
         }
-    });
+    })
 });
 
 module.exports = authRouter;
